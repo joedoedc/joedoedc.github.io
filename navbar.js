18afc41be0ec280de18638d5c14a4cb1180680c5
joedoedc <jseasly@gmail.com>
2023-07-29 16:10:20 -0400

    test

---
+++ navbar.js
@@ -0,0 +1,16 @@
+d3.select("#navbar")
+  .append("a")
+  .text("Home")
+  .attr("href", "Home.html")
+  .style("margin-right", "10px");
+
+d3.select("#navbar")
+  .append("a")
+  .text("Page 1")
+  .attr("href", "Page1.html")
+  .style("margin-right", "10px");
+
+d3.select("#navbar")
+  .append("a")
+  .text("Page 2")
+  .attr("href", "Page2.html");
